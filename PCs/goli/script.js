5cbcf0fa0f4851a5cb9f7926a64fccb55bb7fc42
shaurya770 <89737208+shaurya770@users.noreply.github.com>
2024-06-13 07:34:27 +0530

    Add files via upload

--- PCs/goli/script.js
+++ PCs/goli/script.js
@@ -45,55 +45,49 @@ document.addEventListener('DOMContentLoaded', () => {
       case 'Basic':
         panelInfo.innerHTML = `
           <table id="pcTable">
-    <thead>
-        <tr>
-            <th>Type</th>
-            <th>Name</th>
-            <th>Price</th>
-        </tr>
-    </thead>
+   
     <tbody>
         <tr>
             
             <td>Processor</td>
             <td>i3 10105F</td>
-            <td>6k-8k</td>
+
         </tr>
         <tr>
             
             <td>Graphics Card</td>
             <td>GE Force GT 1030</td>
-            <td>9k-12k</td>
+
         </tr>
         <tr>
             
             <td>Motherboard</td>
             <td>MSI Pro h510m-v</td>
-            <td>6k-8k</td>
+
         </tr>
         <tr>
             
             <td>Ram</td>
             <td>corsair vengeance ddr4(x1)</td>
-            <td>2k-3.5k</td>
+
         </tr>
         <tr>
             
             <td>SSD</td>
             <td>nvme m.2 256gb</td>
-            <td>1.2-2k</td>
+
         </tr>
         <tr>
             
             <td>Secondary Storage</td>
             <td>kingster</td>
-            <td>₹1550</td>
+            
         </tr>
         <tr>
             
             <td>Power Supply Unit</td>
             <td>ANTE SORTS VS 450L</td>
-            <td>₹3902</td>
+            
         </tr>
     </tbody>
 </table>
@@ -106,59 +100,51 @@ document.addEventListener('DOMContentLoaded', () => {
        case 'Gamer':
         panelInfo.innerHTML = `
           <table id="pcTable">
-        <thead>
-            <tr>
-                <th>Type</th>
-                <th>Name</th>
-                <th>Price</th>
-                
-                
-            </tr>
-        </thead>
+        
         <tbody>
             <tr data-price="500">
                 <td>Processor</td>
                 <td>intel i3 10105F</td>
-                <td>6k-8k</td>
+                
                 
                 
             </tr>
             <tr data-price="250">
                 <td>Graphics Card</td>
                 <td>RX560</td>
-                <td>9k-12k</td>
+                
                 
             </tr>
             <tr data-price="100">
                 <td>Motherboard</td>
                 <td>gigabyte h410 m s2 v 2</td>
-                <td>6k-8k</td>
+
                 
             </tr>
 
             <tr data-price="100">
                 <td>Ram</td>
                 <td>corsair vengeance ddr4(x1)</td>
-                <td>2k-3.5k</td>
+
                 
             </tr>
 
             <tr data-price="100">
                 <td>SSD</td>
                 <td>intel i3 10105F</td>
-                <td>1.2k-2k</td>
+
                 
             </tr> 
             <tr data-price="100">
                 <td>Secondary Storage</td>
                 <td>kingster</td>
-                <td>1.2k-2k</td>
+
                 
             </tr>
             <tr data-price="100">
                 <td>Power Supply Unit</td>
                 <td>CSK 550</td>
-                <td>₹3902</td>
+
                 
             </tr>
         </tbody>
@@ -170,43 +156,37 @@ document.addEventListener('DOMContentLoaded', () => {
         case 'Workhouse':
         panelInfo.innerHTML = `
          <table id="pcTable">
-    <thead>
-        <tr>
-            <th>Type</th>
-            <th>Name</th>
-            <th>Price</th>
-        </tr>
-    </thead>
+    
     <tbody>
         <tr data-price="9500">
             <td>Processor</td>
             <td>AMD Ryzen 3 3200G</td>
-            <td>₹9,500</td>
+
         </tr>
         <tr data-price="10000">
             <td>Motherboard</td>
             <td>MSI B550M PRO-VDH WiFi</td>
-            <td>₹10,000</td>
+
         </tr>
         <tr data-price="6000">
             <td>RAM</td>
             <td>16GB (2x8GB) DDR4 3000MHz</td>
-            <td>₹6,000</td>
+
         </tr>
         <tr data-price="4000">
             <td>Storage</td>
             <td>512GB NVMe M.2 SSD</td>
-            <td>₹4,000</td>
+
         </tr>
         <tr data-price="2000">
             <td>CPU Cooler</td>
             <td>Cooler Master Hyper 212 LED</td>
-            <td>₹2,000</td>
+
         </tr>
         <tr data-price="3000">
             <td>Power Supply</td>
             <td>550W 80+ Bronze</td>
-            <td>₹3,000</td>
+
         </tr>
     </tbody>
 </table>
@@ -218,59 +198,51 @@ document.addEventListener('DOMContentLoaded', () => {
          case 'Future-Proof':
         panelInfo.innerHTML = `
           <table id="pcTable">
-        <thead>
-            <tr>
-                <th>Type</th>
-                <th>Name</th>
-                <th>Price</th>
-                
-                
-            </tr>
-        </thead>
+        
         <tbody>
             <tr data-price="500">
                 <td>Processor</td>
                 <td>intel i3 10105F</td>
-                <td>6k-8k</td>
+
                 
                 
             </tr>
             <tr data-price="250">
                 <td>Graphics Card</td>
                 <td>RX560</td>
-                <td>9k-12k</td>
+
                 
             </tr>
             <tr data-price="100">
                 <td>Motherboard</td>
                 <td>gigabyte h410 m s2 v 2</td>
-                <td>6k-8k</td>
+
                 
             </tr>
 
             <tr data-price="100">
                 <td>Ram</td>
                 <td>corsair vengeance ddr4(x1)</td>
-                <td>2k-3.5k</td>
+
                 
             </tr>
 
             <tr data-price="100">
                 <td>SSD</td>
                 <td>intel i3 10105F</td>
-                <td>1.2k-2k</td>
+
                 
             </tr> 
             <tr data-price="100">
                 <td>Secondary Storage</td>
                 <td>kingster</td>
-                <td>1.2k-2k</td>
+
                 
             </tr>
             <tr data-price="100">
                 <td>Power Suply Unit</td>
                 <td>CSK 550</td>
-                <td>Rs 3902</td>
+
                 
             </tr>
         </tbody>
